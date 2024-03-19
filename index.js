5469a9a8da429290c9771bc0ce774c50e4e1666f
Linet <teweilinet@gmail.com>
2024-03-19 11:13:44 +0300

    Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1=31
+let num2=2
+let multiply= num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random ()+1)
+
+let num3=19
+let num4=5
+let mod= num3 % num4
+
+const num=(2,4,6,8,10,12,14,16,18,20)
+const max= 20
+
+ 
